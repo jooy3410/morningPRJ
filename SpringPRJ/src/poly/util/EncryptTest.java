8420dec1239e6dbe7f863c7d8275edbb2b3a4cc9
jooy3410 <jooy3410@naver.com>
2021-05-10 21:33:50 +0900

    해시암호화 알고리즘

---
+++ SpringPRJ/src/poly/util/EncryptTest.java
@@ -0,0 +1,21 @@
+package poly.util;
+
+public class EncryptTest {
+	
+	public static void main(String[] args) throws Exception {
+		
+		System.out.println("-----------------------------------");
+		System.out.println("해시 암호화 알고리즘");
+		// 암호화 문자열
+		String str = "암호화할 문자열";
+		
+		//복호화가 불가능한 해시암호화 알고리즘 실행
+		String hashEnc = EncryptUtil.encHashSHA265(str);
+		
+		//해시 암호화 알고리즘 결과 출력
+		System.out.println("hashEnc : "+ hashEnc);
+		
+		System.out.println("----------------------------------");
+	}
+
+}
